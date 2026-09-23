c029d572b4fec54ef6794e906546d7bd6c19bcb7
Vinutha <vinuthaup@gmail.com>
2026-09-24 00:42:01 +0530

    Prepare Flask app for deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
 from urllib.parse import quote
@@ -855,7 +856,7 @@ if __name__ == "__main__":
     create_database()
 
     app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000)),
+    debug=False
+)
